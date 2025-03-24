92ff2e7a0bbe66c43dcec277578b1a8f350f1576
Haryomidey <fajembolaayo494@gmail.com>
2025-03-24 16:12:03 +0100

    feature main

---
+++ server/routes/productRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { allProducts, createProduct, product, products } from "../controllers/productController.js";
+const router = express.Router();
+
+// post request
+router.post("/create",createProduct);
+// insert many
+router.post('/products',products)
+// all products
+router.get("/all-products",allProducts)
+// single products
+router.get("/:productId",product)
+
+export default router;
